321fb018c34c2bdf70af38fce2567aecd471f14d
Chris Aquino <chris.aquino@gmail.com>
2018-07-09 14:00:02 -0400

    adds basic server

---
+++ index.js
@@ -0,0 +1,9 @@
+// step 1: create express app
+const express = require('express');
+const app = express();
+// step 2: create plain HTTP server
+const http = require('http');
+const server = http.createServer(app);
+// step 3: connect websocket server to http
+const WebSocket = require('ws');
+const wss = new WebSocket.Server();
